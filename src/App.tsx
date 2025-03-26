995cf73f0415f6b5ad4436b43ad811b97ddbace4
Priyanshi singh <priyanshi.singh833@gmail.com>
2025-03-26 01:28:11 -0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ if (isMobile) {
 } else {
   return (
     <>
-        <Router basename="/My-Portfolio">
+        <Router basename="/Myportfolio">
           <div className='tab-container'>
             <Tabs>
               <Tab label="Home" component={Link} to={"/"} sx={{ fontWeight: 'bold', color: 'white', backgroundColor: 'black', zIndex: 1 }} />
